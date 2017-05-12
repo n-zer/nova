5eb6d94cbbce81ae0040ef09e33036a68db3c2a8
Mike Teixeira <miketeixeira95@gmail.com>
2017-05-11 20:25:33 -0400

    Adding static thread_local CriticalLock
    This means we don't have to enter and leave the critical section everytime we pop a job, saving us about 1.5% on our tests. Massive, I know.

--- QueueWrapper.h
+++ QueueWrapper.h
@@ -20,7 +20,7 @@ private:
 	CriticalLock(const CriticalLock&) = delete;
 
 private:
-	CRITICAL_SECTION& m_cs;
+	CRITICAL_SECTION & m_cs;
 };
 
 struct CriticalWrapper {
@@ -43,7 +43,6 @@ public:
 	}
 
 	void Pop(T& item) {
-		CriticalLock lock(s_cs.cs);
 		unsigned counter = 0;
 		while (!m_queue.try_dequeue(item)) {
 			if (counter++ > SPIN_COUNT) {
@@ -73,6 +72,7 @@ private:
 	moodycamel::ConcurrentQueue<T> m_queue;
 	static CONDITION_VARIABLE s_cv;
 	static thread_local CriticalWrapper s_cs;
+	static thread_local CriticalLock s_cl;
 };
 
 template<typename T>
@@ -80,3 +80,6 @@ CONDITION_VARIABLE QueueWrapper<T>::s_cv;
 
 template<typename T>
 thread_local CriticalWrapper QueueWrapper<T>::s_cs;
+
+template<typename T>
+thread_local CriticalLock QueueWrapper<T>::s_cl(QueueWrapper<T>::s_cs);
