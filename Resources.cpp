7d8da7810a7c7b7970fc6109c062266ee9b82203
Mike Teixeira <miketeixeira95@gmail.com>
2017-05-23 00:53:34 -0400

    Missed a file somehow

--- Resources.cpp
+++
@@ -1,9 +0,0 @@
-#include "Resources.h"
-
-namespace Nova {
-	namespace internal{
-		moodycamel::BlockingConcurrentQueue<Envelope> Resources::m_queue;
-		thread_local std::vector<LPVOID> Resources::m_availableFibers;
-		thread_local std::vector<Envelope> Resources::m_currentJobs;
-	}
-}
