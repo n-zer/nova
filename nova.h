1097efda6e213c58a5487ac76b00481bc15dcb96
Mike Teixeira <miketeixeira95@gmail.com>
2017-06-02 12:47:13 -0400

    Adding full invoke implementation

--- nova.h
+++ nova.h
@@ -20,16 +20,41 @@ namespace nova {
 
 #pragma region helpers
 
-		template <class F, class... Args>
-		inline auto invoke(F&& f, Args&&... args) ->
-			decltype(std::forward<F>(f)(std::forward<Args>(args)...)) {
-			return std::forward<F>(f)(std::forward<Args>(args)...);
-		}
+		namespace detail {
+			template <class F, class... Args>
+			inline auto INVOKE(F&& f, Args&&... args) ->
+				decltype(std::forward<F>(f)(std::forward<Args>(args)...)) {
+				return std::forward<F>(f)(std::forward<Args>(args)...);
+			}
 
-		template <class Base, class T, class Derived>
-		inline auto invoke(T Base::*pmd, Derived&& ref) ->
-			decltype(std::forward<Derived>(ref).*pmd) {
-			return std::forward<Derived>(ref).*pmd;
+			template <class Base, class T, class Derived>
+			inline auto INVOKE(T Base::*pmd, Derived&& ref) ->
+				decltype(std::forward<Derived>(ref).*pmd) {
+				return std::forward<Derived>(ref).*pmd;
+			}
+
+			template <class PMD, class Pointer>
+			inline auto INVOKE(PMD pmd, Pointer&& ptr) ->
+				decltype((*std::forward<Pointer>(ptr)).*pmd) {
+				return (*std::forward<Pointer>(ptr)).*pmd;
+			}
+
+			template <class Base, class T, class Derived, class... Args>
+			inline auto INVOKE(T Base::*pmf, Derived&& ref, Args&&... args) ->
+				decltype((std::forward<Derived>(ref).*pmf)(std::forward<Args>(args)...)) {
+				return (std::forward<Derived>(ref).*pmf)(std::forward<Args>(args)...);
+			}
+
+			template <class PMF, class Pointer, class... Args>
+			inline auto INVOKE(PMF pmf, Pointer&& ptr, Args&&... args) ->
+				decltype(((*std::forward<Pointer>(ptr)).*pmf)(std::forward<Args>(args)...)) {
+				return ((*std::forward<Pointer>(ptr)).*pmf)(std::forward<Args>(args)...);
+			}
+		} // namespace detail
+
+		template< class F, class... ArgTypes>
+		decltype(auto) invoke(F&& f, ArgTypes&&... args) {
+			return detail::INVOKE(std::forward<F>(f), std::forward<ArgTypes>(args)...);
 		}
 
 		namespace detail {
